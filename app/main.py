16235f9942e8da02b03e599b02558eb60e9a40fc
Dhiraj Kumbhar <93609511+imDhiraj@users.noreply.github.com>
2025-06-11 18:29:39 +0530

    added favicon

--- app/main.py
+++ app/main.py
@@ -8,6 +8,12 @@ app = FastAPI()
 #API routes
 app.include_router(router)
 
+@app.get("/favicon.ico")
+async def get_favicon():
+    with open("static/favicon.png", "rb") as f:
+        favicon = f.read()
+    return Response(favicon, media_type="image/x-icon")
+
 # Serve static frontend files
 static_dir = os.path.join(os.path.dirname(__file__), "..", "static")
 app.mount("/", StaticFiles(directory=static_dir, html=True), name="static")
